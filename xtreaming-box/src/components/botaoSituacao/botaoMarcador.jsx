a0e6c7feac9e9147c47ad6db49f6eefed25c520b
Flavio Raposo <flavioraposo.90@gmail.com>
2023-06-13 22:38:45 -0300

    ok

--- xtreaming-box/src/components/botaoSituacao/botaoMarcador.jsx
+++ xtreaming-box/src/components/botaoSituacao/botaoMarcador.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import assistirNormal from "../../assets/imgs/SituacaoSerie/assistirNormal.png";
-//import assistirMarcado from "../../assets/imgs/SituacaoSerie/assistirMarcado.png";
+import assistirMarcado from "../../assets/imgs/SituacaoSerie/assistirMarcado.png";
 import assistidoNormal from "../../assets/imgs/SituacaoSerie/assistidoNormal.png";
-//import assistidoMarcado from "../../assets/imgs/SituacaoSerie/assistidoMarcado.png";
+import assistidoMarcado from "../../assets/imgs/SituacaoSerie/assistidoMarcado.png";
 import "./styleBotaoMarcador.css";
 
 
