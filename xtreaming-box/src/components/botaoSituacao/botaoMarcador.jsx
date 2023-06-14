89ad0eac4000cf348999a6173b78dc2ddab1ed02
Flavio Raposo <flavioraposo.90@gmail.com>
2023-06-13 23:13:38 -0300

    series

--- xtreaming-box/src/components/botaoSituacao/botaoMarcador.jsx
+++ xtreaming-box/src/components/botaoSituacao/botaoMarcador.jsx
@@ -4,18 +4,35 @@ import assistirMarcado from "../../assets/imgs/SituacaoSerie/assistirMarcado.png
 import assistidoNormal from "../../assets/imgs/SituacaoSerie/assistidoNormal.png";
 import assistidoMarcado from "../../assets/imgs/SituacaoSerie/assistidoMarcado.png";
 import "./styleBotaoMarcador.css";
-
+import { useState } from 'react';
 
 
 
 function BotaoMarcador (){
 
+  const [imageSrcDes, setImageSrcDes] = useState(assistirNormal);
+     
+  const handleClickDes = () => {
+    // Altere o estado da imagem quando o botão for clicado
+    setImageSrcDes(assistirMarcado);
+  };
+
+
+   const [imageSrcAss, setImageSrcAss] = useState(assistidoNormal);
+   const handleClickAss = () => {
+    // Altere o estado da imagem quando o botão for clicado
+    setImageSrcAss(assistidoMarcado);
+  };
     
 
     return(
-        <div className="margin-botton">
-            <img className="" src={assistirNormal} alt="Botoes"/> 
-            <img className="" src={assistidoNormal} alt="Botoes"/>
+        <div>
+            <div onClick={handleClickDes}>
+                <img src={imageSrcDes} alt="Desejo Assistir"/> 
+            </div>
+            <div onClick={handleClickAss}>
+                <img src={imageSrcAss} alt="Assistido"/> 
+            </div>
         </div>            
     )
 } 
