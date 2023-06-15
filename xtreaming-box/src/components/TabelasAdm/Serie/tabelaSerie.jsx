b14492c23949db0c67d87c1a2e0ffcb96d317a10
Flavio Raposo <flavioraposo.90@gmail.com>
2023-06-15 20:39:09 -0300

    tabela

--- xtreaming-box/src/components/TabelasAdm/Serie/tabelaSerie.jsx
+++ xtreaming-box/src/components/TabelasAdm/Serie/tabelaSerie.jsx
@@ -1,57 +1,58 @@
 import './styletabelaSerie.css'
-
+import trash1 from "../../../assets/imgs/trash1.png"
 function TabelaSerie (){
     return(
 
-            <div className='total-ser'>
-                <div className="graf-title-ser">
+            <div className='container-graf-ser'>
+                <div>
                 <p className="nome-h3-ser">Tabela Séries</p>
+                <img src= {trash1} className="lixeira-ser"  alt="Lixeira"/>
                 </div>
-                     <div className="container-graf-ser">
-                       <div className="cabec-graf">
-                         <div className="col1">
-                             <p>cód</p>
+                
+                     
+                       <div className="cabec-graf-ser">
+                         <div className="col-tit-ser ">
+                             <p>código</p>
                          </div>
-                         <div className="col2">
+                         <div className="col2-ser">
                              <p>Plataforma</p>
                          </div>
-                         <div className="col3">
+                         <div className="col3-ser">
                              <p>Nome</p>
                          </div>
-                         <div className="col4">
+                         <div className="col4-ser">
                             <p>Gênero</p>
                         </div>
-                        <div className="col5">
+                        <div className="col5-ser">
                             <p>Temp</p>
                         </div>
-                        <div className="col6">
+                        <div className="col6-ser">
                             <p>Sinopse</p>
                         </div>
                        </div>
-                       <div className="corpo-graf">
-                        <div className="col1-1">
-                            <p>001</p>
+                       <div className="corpo-graf-ser">
+                        <div className="col-inf-cod">
+                            <p>004444</p>
                         </div>
-                        <div className="col2-1">
+                        <div className="col2-1-ser">
                             <p>Netflix</p>
                         </div>
-                        <div className="col3-1">
+                        <div className="col3-1-ser">
                             <p>Viking</p>
                         </div>
-                        <div className="col4-1">
+                        <div className="col4-1-ser">
                            <p>Ação</p>
                        </div>
-                       <div className="col5-1">
+                       <div className="col5-1-ser">
                            <p>6</p>
                        </div>
-                       <div className="col6-1">
+                       <div className="col6-1-ser">
                             <p>baseado...</p>
                          </div>
-                         <div className="lixeira">
-                            <img src="xtreaming-box/src/assets/imgs/trash1.png" alt=""/>
-                        </div>
+                         
+                        
                        </div>
-                     </div>
+                     
                   </div>
                   
 
