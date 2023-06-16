082d50c1490961eaad6d81373614e7dde3c64087
Mateus Sena Almeida <86931085+mSeTT55@users.noreply.github.com>
2023-06-16 19:34:53 -0300

    Criando minha lista

--- xtreaming-box/src/pages/paginas_Internas/minhaLista/index.jsx
+++ xtreaming-box/src/pages/paginas_Internas/minhaLista/index.jsx
@@ -1,26 +1,67 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './styleMinhaLista.css';
 import './styleMinhaListaMobile.css';
-
 import Interno from '../../../layouts/interno.jsx';
 import exclamacao from '../../../assets/imgs/exclamacao.png';
+import axios from 'axios';
 
-
-function MinhaLista (){ 
-
+function MinhaLista() {
     // Dropdown filtro
-    const [selectedOption, setSelectedOption] = useState(''); 
+    const [selectedOption, setSelectedOption] = useState('');
     const handleSelect = (event) => {
         setSelectedOption(event.target.value);
     };
-    
 
+    const [series, setSeries] = useState([]);
 
-    return( 
-        <Interno>   
+    useEffect(() => {
+        const fetchSeries = async () => {
+            try {
+                const responseSeries = await axios.get('http://localhost:5000/series/get/all');
+                const responseSituacaoSeries = await axios.get('http://localhost:5000/situacao_series/get/all');
+
+                const seriesData = responseSeries.data;
+                const situacaoSeriesData = responseSituacaoSeries.data;
+
+                // Obter ID do usuário logado do Local Storage
+                const userData = JSON.parse(localStorage.getItem('emailFiltrado'));
+                const userId = userData.idusuario;
+
+                // Filtrar séries correspondentes ao usuário logado
+                const filteredSeries = situacaoSeriesData
+                    .filter((situacaoSerie) => situacaoSerie.usuario_idusuario === userId)
+                    .map((situacaoSerie) => {
+                        const serie = seriesData.find((serie) => serie.idseries === situacaoSerie.series_idseries);
+
+                        if (situacaoSerie.desejo_assistir === 'Sim' && situacaoSerie.assistido === 'Sim') {
+                            serie.status = 'Já assisti e quero ver novamente';
+                        } else if (situacaoSerie.desejo_assistir === 'Sim') {
+                            serie.status = 'Desejo Assistir';
+                        } else if (situacaoSerie.assistido === 'Sim') {
+                            serie.status = 'Assistido';
+                        } else {
+                            serie.status = serie.assistido;
+                        }
+
+                        return serie;
+                    });
+
+                setSeries(filteredSeries);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchSeries();
+    }, []);
+
+    return (
+        <Interno>
             <div className="text-botoes-minhalista">
                 <div id="esquerda-text-botoes-minhalista">
-                    <h1 id="minhalista" className="minhalista">Minha Lista</h1>
+                    <h1 id="minhalista" className="minhalista">
+                        Minha Lista
+                    </h1>
                 </div>
                 <div id="direita-text-botoes-minhalista">
                     <p>Categorias</p>
@@ -33,61 +74,66 @@ function MinhaLista (){
                             <option value="nao-recomendo">Não recomendo</option>
                         </select>
                     </div>
-
-
-
                 </div>
             </div>
             <div className="conteiner-maior">
-
-
-
-                <div className="caixa-itens" id=''>
-                    {/*<!--Itens da direita Vazio DESEJO ASSISTIR-->*/}
-                    <div id="direita-itens-vazio" className="direita-itens-vazio-desejo-assistir">
-                        <img src={exclamacao} alt="Exclamação Imagem"/>
+                <div className="caixa-itens" id="">
+                    {/* Itens da direita Vazio DESEJO ASSISTIR */}
+                    <div
+                        id="direita-itens-vazio"
+                        className="direita-itens-vazio-desejo-assistir"
+                    >
+                        <img src={exclamacao} alt="Exclamação Imagem" />
                         <p>Pensando no que assistir? Veja nossa lista de séries.</p>
                         <a href="">Adicionar Séries</a>
                     </div>
-                
-                    {/*<!--Aqui termina toda parte do conteúdo lista quando esta vazia-->*/}
 
+                    {/* Aqui termina toda parte do conteúdo lista quando esta vazia */}
 
-                    {/*<!--Itens da direita Desejo Assistir-->*/}
-                    <div id="direita-itens-desejo-assistir" class="direita-itens-desejo-assistir">
-                        <ul class="item">
-                            <div id="imagem-plataforma">
-                                <img src="/imgs/globo-play.png"/>
-                            </div>                          
-                            <div id="div-nome-serie">
-                                <p class="verde">Série</p>
-                                <p>Viking</p>
-                            </div>
-                            <div  id="div-genero">
-                                <p class="verde">Gênero</p>
-                                <p>Ação</p>
-                            </div>
-                            <div id="div-qntdtemporada">
-                                <p class="verde">Temporadas</p>
-                                <p>6 temporadas</p>
-                            </div>
-                            <div id="div-qntdtemporada">
-                                <p class="verde">Status</p>
-                                <p>assistido</p>
-                            </div>    
-                            <div>
-                                <p class="verde">Recomendado</p>
-                                <div className="recomendado">
-                                    <p>Sim</p><input type="checkbox" id="checkbox-item"/>
-                                    <p>Não</p><input type="checkbox" id="checkbox-item"/>
+                    {/* Itens da direita Desejo Assistir */}
+                    <div
+                        id="direita-itens-desejo-assistir"
+                        className="direita-itens-desejo-assistir"
+                    >
+                        {series.map((serie) => (
+                            <ul className="item" key={serie.idseries}>
+                                <div id="imagem-plataforma">
+                                    <img src="/imgs/globo-play.png" alt="Plataforma" />
                                 </div>
-                            </div>
-                        </ul>
-
-
+                                <div id="div-nome-serie">
+                                    <p className="verde">Série</p>
+                                    <p>{serie.nome}</p>
+                                </div>
+                                <div id="div-genero">
+                                    <p className="verde">Gênero</p>
+                                    <p>{serie.genero}</p>
+                                </div>
+                                <div id="div-qntdtemporada">
+                                    <p className="verde">Temporadas</p>
+                                    <p>{serie.temporada} temporadas</p>
+                                </div>
+                                <div>
+                                    <p className="verde">Status</p>
+                                    <p>{serie.status}</p>
+                                </div>
+                                <div>
+                                    <p className="verde">Recomendado</p>
+                                    <div className="recomendado">
+                                        <label>
+                                            <input type="checkbox" id="checkbox-item" />
+                                            Sim
+                                        </label>
+                                        <label>
+                                            <input type="checkbox" id="checkbox-item" />
+                                            Não
+                                        </label>
+                                    </div>
+                                </div>
+                            </ul>
+                        ))}
                     </div>
                 </div>
-            </div>         
+            </div>
         </Interno>
     );
 }
