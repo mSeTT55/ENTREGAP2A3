b8c7cddfc7470e12ead4241cb0e593d259d99525
Flavio Raposo <flavioraposo.90@gmail.com>
2023-06-17 16:19:22 -0300

    deletar plat

--- xtreaming-box/src/pages/Administrador/Plataforma/DeletarPlataforma/index.jsx
+++ xtreaming-box/src/pages/Administrador/Plataforma/DeletarPlataforma/index.jsx
@@ -2,105 +2,106 @@ import React, { useState } from 'react';
 import './styleplataformaMobile.css';
 import './styleplataforma.css';
 import InternoAdm from '../../../../layouts/internoAdm.jsx';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 
 
 
-    const DeletarPlata = () => {
-        //UseState para pegar os dados do formulario, decompor em um array e aplicar cada dado em cada variavel
-        const [dadosFormPlata, setdadosFormPlata] = useState({
-            
-            nome_plata: '', 
-            imagem_Plata:''
-            
-        });
-      
-        const formSubmitPlata = async (pegarCada) => {
-            //Cancelando o comportamento padrão de recarregar a pagina
-            pegarCada.preventDefault();
-            
-            //montando os dados do array
-            const montandoDadosPlataforma = {
-                
-                nome: dadosFormPlata.nome_plata,
-                imagemplataforma : dadosFormPlata.imagem_Plata
-                
-              };
-      
-            //Inserindo dados da mensagem no banco
-            try {
-                const responsePlataforma = await axios.post('http://localhost:5000/plataformas/post/novo', montandoDadosPlataforma);
-                console.log(responsePlataforma.data);
-                if (responsePlataforma.status === 200){
-                    alert('Plataforma cadastrada com sucesso.');
-                } else{
-                    alert('Erro ao enviar a cadastro, favor tentar novamente.');
-                }
-            } catch (error) {
-                console.log(error);
+const DeletarPlata = () => {
+
+    const navigate = useNavigate();
+
+    //UseState para pegar os dados do formulario, decompor em um array e aplicar cada dado em cada variavel
+    const [dadosFormPlatDel, setdadosFormPlatDel] = useState({
+
+        id_plata: ''
+
+
+    });
+
+    const formSubmitPlata = async (pegarCada) => {
+        //Cancelando o comportamento padrão de recarregar a pagina
+        pegarCada.preventDefault();
+
+
+        // Deletar usuário
+        const deletePlata = async () => {
+
+        }
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const montandoDados = {
+            idplataforma: dadosFormPlatDel.id_plata
+        };
+
+        //Inserindo dados da mensagem no banco
+        try {
+            const response = await axios.delete(`http://localhost:5000/plataforma/delete/${montandoDados.idplataforma}`, {
+                data: JSON.stringify(montandoDados), headers: config.headers
+            });
+            if (response.status === 200) {
+                alert('Plataforma deletada com sucesso, você será redirecionado para a página de cadastro caso mude de ideia.');
+                navigate('/paineldecontrole');
+            } else {
+                alert('Erro ao tentar deletar plataforma');
+
             }
         }
-  return (
-   
-   <InternoAdm>
+        catch (error) {
+             alert('Coloque uma existente na lista de plataforma');
+        }
+    }
+    return (
+
+        <InternoAdm>
             <div className="text-botoes">
                 <div>
-                    <h1 id='editarperfil'  className="minhalista">Plataforma</h1>
+                    <h1 id='editarperfil' className="minhalista">Plataforma</h1>
                 </div>
-   
+
             </div>
             <div className="conteiner-maior">
                 <div className="caixa-itens">
                     <div>
-                    <div>
-                       <p className="nome-h2">Deletar</p>
-                     </div>
-                     <div className="itens-tab">
-                     <form onSubmit={(pegarCada) => formSubmitPlata(pegarCada)}>
-                     <div className="input-box">
-                     <select  name="selec-plata" id="select-plata" placeholder="Selecione a plataforma do streaming">
-                               <option value="1">Netflix</option>
-                                <option value="2">Globo Play</option>
-                                <option value="3">Prime Vídeo</option>
-                                <option value="4">Disney Plus</option>
-                            </select>
-                      </div>      
-                        <div className="linha1-plat">
-                            <div className="input-box">
-                            <input type="text" name="nome" id= "plat_nome" placeholder="   Digite o nome da plataforma de streaming"
-                            required value={dadosFormPlata.nome_plata}
-                            onChange={(pegarCada) => setdadosFormPlata({ ...dadosFormPlata, nome_plata: pegarCada.target.value })}/> 
-                            </div>
-                         </div> 
-                         <div className="linha1-plat" >  
-                            <div className="input-box">
-                                <input type="text" name="inserir-img" id= "plat_img" placeholder="  Digite a url da imagem da TAG da plataforma"
-                                required value={dadosFormPlata.imagem_Plata}
-                                onChange={(pegarCada) => setdadosFormPlata({ ...dadosFormPlata, imagem_Plata: pegarCada.target.value })}/>  
-                            </div> 
-                          </div>
-                            
-                            <div className='format-bt'>
-                                
-                                     <button className="bt-excluir" id="" type="submit">Excluir</button>
-                                
-                                     <Link to={"/paineldecontrole"}>
-                                     <button className="bt-voltar" id="" type="">Voltar</button>
-                                     </Link>
-                            </div>      
-                         </form>       
+                        <div>
+                            <p className="nome-h2">Deletar</p>
                         </div>
-                      </div>                         
+                        <div className="itens-tab">
+                            <form onSubmit={(pegarCada) => formSubmitPlata(pegarCada)}>
+
+                                <div className="linha1-plat">
+                                    <div className="input-box">
+                                        <input type="text" name="nome" id="plat_id" placeholder="   Digite a ID para excluir a plataforma"
+                                            required value={dadosFormPlatDel.id_plata}
+                                            onChange={(pegarCada) => setdadosFormPlatDel({ ...dadosFormPlatDel, id_plata: pegarCada.target.value })} />
+                                    </div>
+                                </div>
+
+
+                                <div className='format-bt'>
+
+                                    <button className="bt-excluir" id="" type="submit">Excluir</button>
+
+                                    <Link to={"/paineldecontrole"}>
+                                        <button className="bt-voltar" id="" type="">Voltar</button>
+                                    </Link>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    
-                    
-                      
-                </div> 
-                
-   </InternoAdm>         
-  )
+                </div>
+
+
+
+            </div>
+
+        </InternoAdm>
+    )
 }
 
 export default DeletarPlata;
